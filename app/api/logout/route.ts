92263c370684678f5c35366738807a60b758b75b
Samir3200 <samirelorf@yahoo.fr>
2026-03-29 16:32:09 +0200

    test disconnected

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   // Supprime le cookie user_id
-  const response = NextResponse.redirect(new URL("/login", req.url));
+  const response = NextResponse.redirect(new URL("/login", req.url), 303);
   response.cookies.set("user_id", "", { path: "/", maxAge: 0 });
   return response;
 }
